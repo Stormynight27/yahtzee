36c834cfc1c09ea2ecd4fd92bb1399d83640ec73
Stormynight27 <76129213+Stormynight27@users.noreply.github.com>
2020-12-16 15:21:40 -0500

    Add files via upload

---
+++ player.py
@@ -0,0 +1,68 @@
+from scoresheet import ScoreSheet
+from hands import YahtzeeHand
+
+
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+
+    x = 13
+    points = []
+
+    diction = {'ones': ScoreSheet().score_ones, 'twos': ScoreSheet().score_twos,
+               'threes': ScoreSheet().score_threes, 'fours': ScoreSheet().score_fours,
+               'fives': ScoreSheet().score_fives, 'sixes': ScoreSheet().score_sixes,
+               'three of a kind': ScoreSheet().score_three_kind, 'four of a kind': ScoreSheet().score_four_kind,
+               'yahtzee': ScoreSheet().score_yahtzee, 'full house': ScoreSheet().score_full_house,
+               'small straight': ScoreSheet().score_small_straight,
+               'large straight': ScoreSheet().score_large_straight, 'chance': ScoreSheet().score_chance
+               }
+    dictionary = diction.copy()
+
+    def turn(self):
+        pass
+
+    def game(self):
+
+        while self.x > 0:
+            hand = YahtzeeHand()
+            hand.reroll()
+
+            score = input(
+                "score this hand, if you want to see which scoring methods you have not used type 'score' instead.\n").lower()
+
+            if score == 'score':
+                self.score_remaining()
+                print(hand)
+                score = input(
+                    "\nscore this hand, by typing one of the scores above\n").lower()
+
+            if score in self.dictionary:
+                self.points.append(self.dictionary[score](hand))
+                if score != 'yahtzee':
+                    del self.dictionary[score]
+
+
+
+            elif score == 'stop':
+                self.x = 0
+                break
+
+            else:
+                for key, value in self.dictionary.items():
+                    print(key)
+                print("are the remaining scoring methods you may use, please properly score it next time")
+
+            self.x -= 1
+            print(self.points)
+
+        print(f'{self.name} has a total of {sum(self.points)}, the game has ended!')
+
+    def score_remaining(self):
+        for key, value in Player.dictionary.items():
+            print(key)
+
+
+player = Player("player1")
+player.game()
---
+++ scoresheet.py
@@ -0,0 +1,75 @@
+class ScoreSheet:
+
+    def _score_pairs(self, hand, set_size):
+        if set_size == 3 or set_size == 4:
+            for worth, count, in hand._sets.items():
+                if count == set_size:
+                    return sum(hand)
+        elif set_size == 5:
+            for worth, count, in hand._sets.items():
+                if count == set_size:
+                    return sum(hand) + 50
+
+    def _score_house(self, hand):
+        for worth, count, in hand._sets.items():
+            if count == 3:
+                for value, repeat, in hand._sets.items():
+                    if repeat == 2:
+                        return sum(hand) + 25
+
+    def _score_l_straight(self, hand):
+        for worth, count, in hand._sets.items():
+            if count == 1:
+                if hand.sixes == [] or hand.ones == []:
+                    return 40
+
+    def _score_s_straight(self, hand):
+        if hand._sets[4] == 1 and hand._sets[5] == 1 and hand.sixes != [] and hand.threes != []:
+            return 30
+        if hand._sets[3] == 1 and hand._sets[4] == 1 and hand.twos != [] and hand.fives != []:
+            return 30
+        elif hand._sets[2] == 1 and hand._sets[3] == 1 and hand.ones != [] and hand.fours != []:
+            return 30
+
+    def _score_chance(self, hand):
+        return sum(hand)
+
+
+    def score_ones(self, hand):
+        return sum(hand.ones)
+
+    def score_twos(self, hand):
+        return sum(hand.twos)
+
+    def score_threes(self, hand):
+        return sum(hand.threes)
+
+    def score_fours(self, hand):
+        return sum(hand.fours)
+
+    def score_fives(self, hand):
+        return sum(hand.fives)
+
+    def score_sixes(self, hand):
+        return sum(hand.sixes)
+
+    def score_three_kind(self, hand):
+        return self._score_pairs(hand, 3)
+
+    def score_four_kind(self, hand):
+        return self._score_pairs(hand, 4)
+
+    def score_yahtzee(self, hand):
+        return self._score_pairs(hand, 5)
+
+    def score_full_house(self, hand):
+        return self._score_house(hand)
+
+    def score_large_straight(self, hand):
+        return self._score_l_straight(hand)
+
+    def score_small_straight(self, hand):
+        return self._score_s_straight(hand)
+
+    def score_chance(self, hand):
+        return self._score_chance(hand)
